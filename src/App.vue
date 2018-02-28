dca4e017f5d3627f0a7e49948f505eddbc0c554a
xionghuchao <xionghuchao@qq.com>
2018-02-28 16:40:47 +0800

    测试123

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
   export default {
     name: 'APP',
     created() {
+      console.debug('测试ewe')
+      console.debug('测weqwewq试')
       console.debug('测试')
-      console.debug('测试')
-      console.debug('测试')
-      console.debug('测试')
+      console.debug('测试124')
     }
   }
 </script>
